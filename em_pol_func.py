5f11bbcb1be6b16de2810c660e5eae341f2888f0
Muhammad Sujak MK <anack.magja@gmail.com>
2026-02-18 01:08:22 +0900

    Update em_pol_func.py

--- em_pol_func.py
+++ em_pol_func.py
@@ -4,23 +4,17 @@ import mpmath as mp
 
 mp.mp.dps = 80
 
-def safe_div(num, den, eps=mp.mpf('1e-60')):
-    if abs(den) < eps:
-        den = den + eps*(1+1j)
-    return num/den
-
-
 def alpha_em(n_med, wv, r, n=1, wv_scale = 1e-9):
     wv = mp.mpf(wv) 
     r = mp.mpf(r) 
     n_med = mp.mpf(n_med) 
-    epsd = (n_med)**2 
+    epsd = (n_med)**2
+    eps0 = 8.854e-12 # Unit of F/m
     
     mat_core=mat.mat_cal(wv/wv_scale,r"\Si_Green.txt")
     k0 = 2*mp.pi/wv
     kd = k0*n_med
     x1 = kd*r
-    x1 = x1 + 1j*mp.mpf("1e-30")
     m1 = mp.mpc(mat_core) / n_med
     psi_n, psi_n1 = rc.ricabes1(n, x1)
     zeta_n, zeta_n1 = rc.ricabes3(n, x1)
@@ -30,9 +24,8 @@ def alpha_em(n_med, wv, r, n=1, wv_scale = 1e-9):
     
     b = ((m1*D + n/x1) * psi_n - psi_n1) / ((m1*D + n/x1) * zeta_n - zeta_n1)
     #alpha_e = 1j*6*mp.pi*eps0*epsd*a/(kd)**3  #Unit of SI (F m^2) 
-    alpha_e = 1j*6*mp.pi*a/(k0)**3 # Unit of volume (m^3)
-    alpha_m = 1j*6*mp.pi*b/(k0)**3
-    
+    alpha_e = 1j*6*mp.pi*a/(kd)**3 # Unit of volume (1 / m^3)
+    alpha_m = 1j*6*mp.pi*b/(kd)**3
     return alpha_e, alpha_m
 
 
